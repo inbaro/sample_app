129761692231b0381f9bf79eddc30e0a3dcd70b3
inbar <inbar@agilesparks.com>
2012-05-01 14:43:36 +0300

    Male a basic user model

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+class User < ActiveRecord::Base
+    attr_accessible :email, :name, :password, :password_confirmation
+    has_secure_password
+     
+    before_save { |user| user.email = email.downcase }
+
+    validates :name, presence: true, length: { maximum: 50 }
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_PASSWORD_REGEX = /^\w*(?=\w*\d)(?=\w*[a-z])(?=\w*[A-Z])\w*$/
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    
+    validates :password, length: { minimum: 6 }, format: {with: VALID_PASSWORD_REGEX}
+    validates :password, length: { maximum: 12 }
+    
+    validates :password_confirmation, presence: true
+end
